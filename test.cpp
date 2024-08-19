4bdb9224ad3ef426f58c2c699f2247dd8ded39b2
yeyeye2333 <3229833855@qq.com>
2024-08-19 15:48:58 +0800

    readme 更改

--- test.cpp
+++ test.cpp
@@ -6,15 +6,9 @@
 #include<string.h>
 #include</usr/include/openssl/sha.h>
 using std::string;
-void mysha(string &str){
-    unsigned char md[33]={0};
-    SHA256(reinterpret_cast<const unsigned char *>(str.c_str()),str.size(),md);
-    str=reinterpret_cast<char *>(md);
-}
 int main()
 {
     string tmp;
     std::cin>>tmp;
-    mysha(tmp);
     std::cout<<tmp;
 }
